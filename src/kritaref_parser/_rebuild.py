faa0492d4be61940327cea384cafaaecd8e9dd42
gchang12 <gc8@hawaii.edu>
2025-08-30 19:57:27 -0700

    Fixed anchor attributes.

--- src/kritaref_parser/_rebuild.py
+++ src/kritaref_parser/_rebuild.py
@@ -100,6 +100,7 @@ def have_anchor_tags_reference_source2(excerpt_dir):
                 a['class'].remove('internal')
                 a['class'].append('link-to-official-docs')
                 a['href'] = new_href
+                a['rel'] = "external"
             htmlfile.write_text(str(soup), encoding='utf-8')
             file_count += 1
         logging.debug("%d files have been modified for '%s'.", file_count, section)
@@ -132,12 +133,41 @@ def delete_orphaned_figcaption(excerpt_dir):
 
 # specially mark blending mode subsections
 
+def set_rel_attribute(excerpt_dir):
+    """
+    """
+    sections = (
+        "brush_engines/",
+        "tools/",
+        "brush_settings/",
+        "dockers/",
+        "filters/",
+        "layers_and_masks/",
+        "main_menu/",
+        "preferences/",
+        "resource_management/",
+        "blending_modes/",
+    )
+    for section in sections:
+        path_to_section = Path(excerpt_dir, section)
+        file_count = 0
+        for htmlfile in filter(lambda path: path.is_file(), path_to_section.iterdir()):
+            htmltext = htmlfile.read_text(encoding='utf-8')
+            soup = BeautifulSoup(htmltext, 'html.parser')
+            for a in soup.find_all("a"):
+                if "external" in a['class']:
+                    a['class'].remove("external")
+                if a['href'].startswith('http'):
+                    a['rel'] = "external"
+            htmlfile.write_text(str(soup), encoding="utf-8")
+
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.DEBUG,
     )
-    excerpt_dir = "./static/excerpts/"
+    excerpt_dir = "./frontend/kritaref_palette/public/excerpts/"
     #have_anchor_tags_reference_source(excerpt_dir)
-    have_anchor_tags_reference_source2(excerpt_dir)
-    delete_orphaned_figcaption(excerpt_dir)
+    #have_anchor_tags_reference_source2(excerpt_dir)
+    #delete_orphaned_figcaption(excerpt_dir)
+    set_rel_attribute(excerpt_dir)
 
