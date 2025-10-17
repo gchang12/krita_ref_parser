75e28c54c7dd438b7a284719cc35bf5a3d1f96a8
gchang12 <gc8@hawaii.edu>
2025-10-17 09:59:47 -0700

    Fleshed out sample image class

--- src/krita_ref_generator/image_amputator.py
+++ src/krita_ref_generator/image_amputator.py
@@ -5,34 +5,76 @@ import enum
 
 from PIL import Image
 
+from krita_ref_generator.logging import logger
+
 SOURCE_DIR = "../../input/docs-krita-org/_build/html/_images/"
 TARGET_DIR = "../../output/images/"
 
-class SampleImage(enum.StrEnum):
+class SampleImageType(enum.Enum):
     """
     """
+    # VARIOUS
     WITH_DOTS = "_Sample_image_with_dots.png"
     GREY_04_05 = "_Gray_0.4_and_Gray_0.5_n.png"
     LIGHT_BLUE_AND_ORANGE = "_Light_blue_and_orange.png"
+    # MODULO
     GRADIENT_COMPARISON = "_Gradient_Comparison.png"
+    # BINARY
     MAP = "_map.png"
     GRADIENTS = "_Gradients.png"
 
-# TODO: Figure out the rest of the constants.
+    @classmethod
+    def get_sample_image_type(cls, filename: str):
+        """
+        """
+        logger.debug("Checking if '%s' is one of the declared SampleImageType enumerations.", filename)
+        try:
+            matched_sample_image_type = list(filter(lambda image_type: filename.endswith(image_type.value), cls)).pop()
+            logger.debug("'%s' matched the %s SampleImageType enumeration; returning latter.", filename, matched_sample_image_type)
+            return matched_sample_image_type
+        except IndexError as idx_err:
+            logger.warning("'%s' does not fall into one of the declared SampleImageType enumerations.", filename)
+            raise idx_err
+
+    def get_filename_for_default(self):
+        """
+        """
+        prefix = "."
+        filename_for_default_image = prefix + self.value
+        logger.debug("Returning: '%s'", filename_for_default_image)
+        return filename_for_default_image
+
+    def get_number_of_partitions(self):
+        """
+        """
+        return {
+            # (various)
+            self.WITH_DOTS: 2,
+            self.GREY_04_05: 2,
+            self.LIGHT_BLUE_AND_ORANGE: 2,
+            # MODULO
+            self.GRADIENT_COMPARISON: 3,
+            # BINARY
+            self.MAP: 3,
+            self.GRADIENTS: 3,
+        }[self]
 
 def get_half_of_image_file(filename: str, *, get_first_half: bool):
     """
     Returns one of two halves of image specified by `filename`.
     """
     #logger.debug("Now halving: %s", filename)
+    logger.debug("Halving: '%s'.", filename)
     with Image.open(filename) as img:
         half_width = int(img.size[0] / 2)
         full_height = img.size[1]
         full_width = img.size[0]
         # (top, left, right, bottom)
         if get_first_half:
+            logger.debug("Getting first half.")
             box = (0, 0, half_width, full_height)
         else:
+            logger.debug("Getting second half.")
             box = (half_width, 0, full_width, full_height)
         cropped_image = img.crop(box)
     return cropped_image
@@ -40,13 +82,16 @@ def get_half_of_image_file(filename: str, *, get_first_half: bool):
 def get_thirds_of_image_file(filename: str, *, get_last_third: bool):
     """
     """
+    logger.debug("Cutting '%s' into thirds.", filename)
     with Image.open(filename) as img:
         full_height = img.size[1]
         full_width = img.size[0]
         two_thirds_width = int(full_width * 2 / 3)
         if get_last_third:
+            logger.debug("Getting last third.")
             box = (two_thirds_width, 0, full_width, full_height)
         else:
+            logger.debug("Getting first two-thirds.")
             box = (0, 0, two_thirds_width, full_height)
         cropped_image = img.crop(box)
     return cropped_image
@@ -54,10 +99,16 @@ def get_thirds_of_image_file(filename: str, *, get_last_third: bool):
 def delete_unused_images(index):
     """
     """
-    image_files = filter(lambda file: file.is_file(), Path(TARGET_DIR).iterdir())
-    unused_images = filter(lambda file: file.name not in index, image_files)
+    logger.debug("Found (%d) filenames in index.", len(index))
+    image_files = tuple(filter(lambda file: file.is_file(), Path(TARGET_DIR).iterdir()))
+    num_image_files = len(image_files)
+    logger.debug("Found (%d) image files in '%s'.", num_image_files, TARGET_DIR)
+    unused_images = tuple(filter(lambda file: file.name not in index, image_files))
+    num_unused_images = len(unused_images)
+    logger.debug("Found (%d) unused image files in '%s'.", num_unused_images, TARGET_DIR)
     for imagefile in unused_images:
         imagefile.unlink()
+    logger.debug("Deleted images. Number of images remaining: %d", num_image_files - num_unused_images)
 
 if __name__ == "__main__":
     def halve_blendingmode_dots_images():
@@ -68,3 +119,4 @@ if __name__ == "__main__":
             blended_image.save(imagefile)
         og_image = get_half_of_image_file(imagefile, get_first_half=True)
         og_image.save(Path(TARGET_DIR, OG_DOTS_IMAGE))
+
