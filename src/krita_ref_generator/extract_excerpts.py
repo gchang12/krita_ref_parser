db7d9bdc9dc5da40d0585858bb11670eaa58cc2b
gchang12 <gc8@hawaii.edu>
2025-10-17 08:13:09 -0700

    Added logging.

--- src/krita_ref_generator/extract_excerpts.py
+++ src/krita_ref_generator/extract_excerpts.py
@@ -4,6 +4,8 @@ Extracts raw excerpts from HTML copy of Krita documentation.
 
 from bs4 import BeautifulSoup
 
+from krita_ref_generator.logging import logger
+
 SOURCE_DIR = "../../input/docs-krita-org/_build/html/reference_manual/"
 TARGET_DIR = "../../output/excerpts/"
 
@@ -11,17 +13,20 @@ def from_page(soup: BeautifulSoup):
     """
     """
     sections = [soup.css.select_one("section[id]")]
+    logger.debug("(%d) sections found. Returning as list.", len(sections))
     return sections
 
 def from_blendingmodes_page(soup: BeautifulSoup):
     """
     """
     sections = list(soup.css.select("section[id]")[1:])
+    logger.debug("(%d) sections found. Returning as list.", len(sections))
     return sections
 
 def from_hsx_blendingmodes_page(soup: BeautifulSoup):
     """
     """
     sections = list(section.css.select("#hsx-blending-modes > section[id]"))
+    logger.debug("(%d) sections found. Returning as list.", len(sections))
     return sections
 
--- src/krita_ref_generator/soup_scribe.py
+++ src/krita_ref_generator/soup_scribe.py
@@ -3,21 +3,32 @@
 
 from bs4 import BeautifulSoup
 
+from krita_ref_generator.logging import logger
+
 def format_filename_of_target(filename: str, h2_text: str):
     """
     """
+    logger.debug("Old filename: '%s'", filename)
     new_filename = filename.replace('.html', '') \
         + "_" \
         + h2_text.replace(' - ', '-').replace(' ', '-') \
         + ".html"
+    logger.debug("Filename after appending header: '%s'", new_filename)
     new_filename = new_filename.replace(' ', '_').lower()
+    logger.debug("Filename after replacing ' ' with '_': '%s'", new_filename)
+    # remove any characters that are invalid
     new_filename = re.sub("[^a-z0-9_.-]", "", new_filename)
+    logger.debug("Filename after erasing invalid characters: '%s'. Returning: '%s'", new_filename, new_filename)
     return new_filename
 
 def write_stripped_soup(soup: BeautifulSoup, filename: str):
     """
     """
-    soup_as_str = "\n".join([line.strip() for line in str(soup).splitlines() if line.strip()])
-    write_result = Path(filename).write_text(soup_as_str, encoding="utf-8")
+    logger.debug("Writing lines to '%s'. Calculating number of lines.", filename)
+    soup_as_lines = [line.strip() for line in str(soup).splitlines() if line.strip()]
+    soup_as_str = "\n".join(soup_as_lines)
+    logger.debug("(%d) lines were found in soup. Writing.", len(soup_as_lines))
+    Path(filename).write_text(soup_as_str, encoding="utf-8") # returns number of bytes written; unneeded.
+    logger.debug("Write operation successful.")
     return write_result
 
