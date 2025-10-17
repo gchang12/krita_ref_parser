9e2def39fa186d0da455e11de6a29803dd9b8e70
gchang12 <gc8@hawaii.edu>
2025-10-17 08:01:35 -0700

    Fixed typo in soup_scribe module

--- src/krita_ref_generator/soup_scribe.py
+++ src/krita_ref_generator/soup_scribe.py
@@ -6,14 +6,13 @@ from bs4 import BeautifulSoup
 def format_target_filename(filename: str, h2_text: str):
     """
     """
-    #filename_root = path_to_source.name
-    new_filename =  filename.replace('.html', '') \
+    new_filename = filename.replace('.html', '') \
         + "_" \
         + h2_text.replace(' - ', '-').replace(' ', '-') \
         + ".html"
     new_filename = new_filename.replace(' ', '_').lower()
     new_filename = re.sub("[^a-z0-9_.-]", "", new_filename)
-    return filename
+    return new_filename
 
 def write_stripped_soup(soup: BeautifulSoup, filename: str):
     """
@@ -21,3 +20,4 @@ def write_stripped_soup(soup: BeautifulSoup, filename: str):
     soup_as_str = "\n".join([line.strip() for line in str(soup).splitlines() if line.strip()])
     write_result = Path(filename).write_text(soup_as_str, encoding="utf-8")
     return write_result
+
