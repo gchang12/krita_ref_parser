d58f08619337d227cb81ffb3a68b31db79556060
gchang12 <gc8@hawaii.edu>
2025-10-17 17:34:51 -0700

    Modified logging.

--- src/krita_ref_generator/_logging.py
+++ src/krita_ref_generator/_logging.py
@@ -7,7 +7,7 @@ import logging
 logger = logging.getLogger(name="krita_ref_generator")
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     filename="krita_ref_generator.log",
     format="%(levelname)s:%(module)s.%(funcName)s: %(message)s",
     filemode="w",
