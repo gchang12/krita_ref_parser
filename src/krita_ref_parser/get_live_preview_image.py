2f5745035f2ca91ea0effcf4322c0339a4473313
gchang12 <gc8@hawaii.edu>
2026-02-20 00:17:20 -1000

    I cleaned up the preview-image extractor script.

--- src/krita_ref_parser/get_live_preview_image.py
+++ src/krita_ref_parser/get_live_preview_image.py
@@ -3,25 +3,33 @@ from krita import *
 
 # initialize Krita
 K = Krita.instance()
-
-# get all presets
-ALL_PRESETS = K.resources("preset")
-
-# get preset for testing
-name = "b) Airbrush Soft"
-preset = ALL_PRESETS[name]
-
 # open document
-active_window = K.activeWindow()
-active_window.activeView().setCurrentBrushPreset(preset) # TODO: Figure out where this goes.
+ACTIVE_WINDOW = K.activeWindow()
 
-# locate brush previewer
-qmwin = active_window.qwindow()
-presets_editor = qmwin.findChild(QWidget, "KisPaintOpPresetsEditor")
-brush_previewer = presets_editor.findChild(QGraphicsView, "liveBrushPreviewView")
+def save_preset(name: str, preset, directory: str):
+    """
+    """
+    # locate brush previewer
+    qmwin = ACTIVE_WINDOW.qwindow()
+    presets_editor = qmwin.findChild(QWidget, "KisPaintOpPresetsEditor")
+    brush_previewer = presets_editor.findChild(QGraphicsView, "liveBrushPreviewView")
+    # render
+    #brush_previewer.render(QPainter())
+    # extract and save
+    pixmap = brush_previewer.grab()
+    filename = os.path.join(directory, name + '.png')
+    return pixmap.save(filename)
 
-# extract and save
-pixmap = brush_previewer.grab()
-filename = os.path.join(".", "Desktop", "presets", name + '.png')
-pixmap.save(filename)
-
+if __name__ == "__main__":
+    # get all presets
+    ALL_PRESETS = K.resources("preset")
+    # save live-previews of all presets
+    directory = "Desktop/presets"
+    '''
+    for name, preset in ALL_PRESETS.items():
+        ACTIVE_WINDOW.activeView().setCurrentBrushPreset(preset)
+        save_preset(name, preset, directory)
+    '''
+    name = "b) Airbrush Soft"
+    preset = ALL_PRESETS[name]
+    save_preset(name, preset, directory)
