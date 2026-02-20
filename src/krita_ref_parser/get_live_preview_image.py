3fde1b6dd5cc194f2742b9a773dd15bf9f0c1a9b
gchang12 <gc8@hawaii.edu>
2026-02-19 19:46:36 -1000

    I'm trying to figure out where to put that setCurrentBrushPreset line.

--- src/krita_ref_parser/get_live_preview_image.py
+++ src/krita_ref_parser/get_live_preview_image.py
@@ -3,21 +3,23 @@ from krita import *
 
 # initialize Krita
 K = Krita.instance()
+
+# get preset
+name = "b) Airbrush Soft"
+preset = K.resources("preset")[name]
+
 # open document
 document = K.createDocument(1, 1, "GET_BRUSH_PREVIEW", "RGBA", "U8", "", 120.0)
-#active_window = K.activeWindow()
-#view = active_window.addView(document)
-# get preset
-ALL_PRESETS = Application.resources("preset")
-name = "b) Airbrush Soft"
-preset = ALL_PRESETS[name]
-# change brush preset of view
-view = active_window.activeView()
-view.setCurrentBrushPreset(preset)
+active_window = K.activeWindow()
+active_window.addView(document)
+active_view = active_window.activeView()
+
 # locate brush previewer
+active_view.setCurrentBrushPreset(preset)
 qmwin = active_window.qwindow()
 presets_editor = qmwin.findChild(QWidget, "KisPaintOpPresetsEditor")
 brush_previewer = presets_editor.findChild(QGraphicsView, "liveBrushPreviewView")
+
 # extract and save
 pixmap = brush_previewer.grab()
 filename = os.path.join(".", "Desktop", "presets", name + '.png')
